14b46340413c243f2e85b1f5bb0bef1d0ff04761
young <hz.young@qq.com>
2023-09-05 15:43:52 +0800

    热点头条

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmMaterialController.java
@@ -0,0 +1,11 @@
+/**
+ * 1. @ClassName WmMaterialController
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/5 15:42
+ */
+package com.heima.wemedia.controller.v1;
+
+public class WmMaterialController {
+}
+
