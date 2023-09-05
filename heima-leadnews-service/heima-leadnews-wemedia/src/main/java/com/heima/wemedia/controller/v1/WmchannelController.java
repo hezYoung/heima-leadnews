1752123429b7d5a5cb87bcbf7042f807ae90f27a
young <hz.young@qq.com>
2023-09-05 17:37:05 +0800

    热点头条

--- heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmchannelController.java
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/controller/v1/WmchannelController.java
@@ -8,6 +8,8 @@ package com.heima.wemedia.controller.v1;
 
 
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.wemedia.service.WmChannelService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +17,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/channel")
 public class WmchannelController {
-
+    @Autowired
+    private WmChannelService wmChannelService;
 
     @GetMapping("/channels")
     public ResponseResult findAll(){
-        return null;
+        ResponseResult all = wmChannelService.findAll();
+        return ResponseResult.okResult(all);
     }
 }
 
---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/service/WmChannelService.java
@@ -0,0 +1,19 @@
+/**
+ * 1. @ClassName WmChannelService
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/5 17:24
+ */
+package com.heima.wemedia.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.wemedia.pojos.WmChannel;
+
+public interface WmChannelService extends IService<WmChannel> {
+    /**
+     * 查询所有频道
+     * @return
+     */
+    public ResponseResult findAll();
+}
