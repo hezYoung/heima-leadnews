69a113711e3745fca31f4488d9b6d2479968d17c
young <hz.young@qq.com>
2023-09-07 10:41:01 +0800

    热点头条

---
+++ heima-leadnews-common/src/main/java/com/heima/common/constansts/WemediaConstants.java
@@ -0,0 +1,24 @@
+/**
+ * 1. @ClassName WemediaConstants
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/7 10:40
+ */
+package com.heima.common.constansts;
+
+public class WemediaConstants {
+
+    public static final Short COLLECT_MATERIAL = 1;//收藏
+
+    public static final Short CANCEL_COLLECT_MATERIAL = 0;//取消收藏
+
+    public static final String WM_NEWS_TYPE_IMAGE = "image";
+
+    public static final Short WM_NEWS_NONE_IMAGE = 0;
+    public static final Short WM_NEWS_SINGLE_IMAGE = 1;
+    public static final Short WM_NEWS_MANY_IMAGE = 3;
+    public static final Short WM_NEWS_TYPE_AUTO = -1;
+
+    public static final Short WM_CONTENT_REFERENCE = 0;
+    public static final Short WM_COVER_REFERENCE = 1;
+}
