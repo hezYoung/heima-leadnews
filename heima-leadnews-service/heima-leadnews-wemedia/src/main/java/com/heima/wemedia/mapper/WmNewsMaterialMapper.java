ea7b599c16a0c040d0fd19cd26e7085742771338
young <hz.young@qq.com>
2023-09-07 10:36:29 +0800

    热点头条

---
+++ heima-leadnews-service/heima-leadnews-wemedia/src/main/java/com/heima/wemedia/mapper/WmNewsMaterialMapper.java
@@ -0,0 +1,17 @@
+/**
+ * 1. @ClassName WmNewsMaterialMapper
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/7 10:34
+ */
+package com.heima.wemedia.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.heima.model.wemedia.pojos.WmNewsMaterial;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface WmNewsMaterialMapper extends BaseMapper<WmNewsMaterial> {
+    void saveRelations(@Param("materialIds") List<Integer> materialIds, @Param("newsId") Integer newsId, @Param("type")Short type);
+}
