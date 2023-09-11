eff810de54d77cdd3c0165a2ddaafaebe9b01fad
young <hz.young@qq.com>
2023-09-11 20:37:51 +0800

    elaticsearch查询3

--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ArticleSearchController.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/controller/v1/ArticleSearchController.java
@@ -8,6 +8,8 @@ package com.heima.search.controller.v1;
 
 import com.heima.model.common.dtos.ResponseResult;
 import com.heima.model.search.dtos.UserSearchDto;
+import com.heima.search.service.ApArticleSearchService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,11 @@ import java.io.IOException;
 public class ArticleSearchController {
 
 
+    @Autowired
+    private ApArticleSearchService articleSearchService;
+
     @PostMapping("/search")
     public ResponseResult search(@RequestBody UserSearchDto dto) throws IOException {
-        return null;
+        return articleSearchService.search(dto);
     }
 }
--- heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ArticleSearchServiceImpl.java
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/service/impl/ArticleSearchServiceImpl.java
@@ -6,15 +6,32 @@
  */
 package com.heima.search.service.impl;
 
+import com.alibaba.fastjson.JSON;
 import com.heima.model.common.dtos.ResponseResult;
+import com.heima.model.common.enums.AppHttpCodeEnum;
 import com.heima.model.search.dtos.UserSearchDto;
 import com.heima.search.service.ApArticleSearchService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+import org.checkerframework.checker.units.qual.A;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
+import org.elasticsearch.index.query.*;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.sort.SortOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.Map;
+
 @Slf4j
 @Service
 public class ArticleSearchServiceImpl implements ApArticleSearchService {
@@ -22,7 +39,57 @@ public class ArticleSearchServiceImpl implements ApArticleSearchService {
     private RestHighLevelClient restHighLevelClient;
     @Override
     public ResponseResult search(UserSearchDto userSearchDto) throws IOException {
-        return null;
+        //检查参数
+        if (userSearchDto == null || StringUtils.isBlank(userSearchDto.getSearchWords())) {
+            return ResponseResult.errorResult(AppHttpCodeEnum.PARAM_INVALID);
+        }
+        //设置es的查询条件
+        SearchRequest appInfoArticle = new SearchRequest("app_info_article");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        //布尔查询
+        BoolQueryBuilder boolQueryBuilder = QueryBuilders.boolQuery();
+        //分词查询
+        QueryStringQueryBuilder queryStringQueryBuilder = QueryBuilders.queryStringQuery(userSearchDto.getSearchWords()).field("title").field("content").defaultOperator(Operator.OR);
+        boolQueryBuilder.must(queryStringQueryBuilder);
+        //查询小于mindate的数据
+        RangeQueryBuilder publishTime = QueryBuilders.rangeQuery("publishTime").lt(userSearchDto.getMinBehotTime().getTime());
+        boolQueryBuilder.filter(publishTime);
+        //分页查询
+        searchSourceBuilder.from(0);
+        searchSourceBuilder.size(userSearchDto.getPageSize());
+        //按照发布时间倒序查询
+        searchSourceBuilder.sort("publishTime", SortOrder.DESC);
+        //设置高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.field("title");
+        highlightBuilder.preTags("<font style='color: yellow; font-size: inherit;'>");
+        highlightBuilder.postTags("</font>");
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        //组装查询
+        searchSourceBuilder.query(boolQueryBuilder);
+        appInfoArticle.source(searchSourceBuilder);
+        SearchResponse searchResponse = restHighLevelClient.search(appInfoArticle, RequestOptions.DEFAULT);
+        //结果封装
+        ArrayList<Map> list = new ArrayList<>();
+        SearchHit[] hits = searchResponse.getHits().getHits();
+        for (SearchHit hit : hits) {
+            String sourceAsString = hit.getSourceAsString();
+            Map map = JSON.parseObject(sourceAsString, Map.class);
+            //高亮
+            if (hit.getHighlightFields() != null && hit.getHighlightFields().size() > 0) {
+                Text[] titles = hit.getHighlightFields().get("title").getFragments();
+                String join = StringUtils.join(titles);
+                map.put("h_title", join);
+
+            }else {
+                //原始标题
+                map.put("h_title", map.get("title"));
+            }
+            list.add(map);
+        }
+        return ResponseResult.okResult(list);
     }
+
 }
 
