a3fddf07e1bd45bfee714e98bb06adcaf11985e5
young <hz.young@qq.com>
2023-09-12 10:40:21 +0800

    mongodb实现历史记录

---
+++ heima-leadnews-service/heima-leadnews-search/src/main/java/com/heima/search/pojos/ApUserSearch.java
@@ -0,0 +1,47 @@
+/**
+ * 1. @ClassName ApUserSearch
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/12 10:37
+ */
+package com.heima.search.pojos;
+
+import lombok.Data;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * <p>
+ * APP用户搜索信息表
+ * </p>
+ * @author itheima
+ */
+@Data
+@Document("ap_user_search")
+public class ApUserSearch implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    private String id;
+
+    /**
+     * 用户ID
+     */
+    private Integer userId;
+
+    /**
+     * 搜索词
+     */
+    private String keyword;
+
+    /**
+     * 创建时间
+     */
+    private Date createdTime;
+
+}
