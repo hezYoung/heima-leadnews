578985ba8dbb8356b418fdfef05e7b9d0e1d2648
young <hz.young@qq.com>
2023-09-14 09:34:57 +0800

    定时计算xxl-job

---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/config/XxlJobConfig.java
@@ -0,0 +1,46 @@
+/**
+ * 1. @ClassName XxlJobConfig
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/14 9:31
+ */
+package com.heima.article.config;
+
+import com.xxl.job.core.executor.impl.XxlJobSpringExecutor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * xxl-job config
+ *
+ * @author xuxueli 2017-04-28
+ */
+@Configuration
+public class XxlJobConfig {
+    private Logger logger = LoggerFactory.getLogger(XxlJobConfig.class);
+
+    @Value("${xxl.job.admin.addresses}")
+    private String adminAddresses;
+
+    @Value("${xxl.job.executor.appname}")
+    private String appname;
+
+    @Value("${xxl.job.executor.port}")
+    private int port;
+
+
+    @Bean
+    public XxlJobSpringExecutor xxlJobExecutor() {
+        logger.info(">>>>>>>>>>> xxl-job config init.");
+        XxlJobSpringExecutor xxlJobSpringExecutor = new XxlJobSpringExecutor();
+        xxlJobSpringExecutor.setAdminAddresses(adminAddresses);
+        xxlJobSpringExecutor.setAppname(appname);
+        xxlJobSpringExecutor.setPort(port);
+        return xxlJobSpringExecutor;
+    }
+
+
+}
---
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/job/ComputeHotArticleJob.java
@@ -0,0 +1,29 @@
+/**
+ * 1. @ClassName ComputeHotArticleJob
+ * 2. @Description TODO
+ * 3. @Author Young
+ * 4. @Date 2023/9/14 9:32
+ */
+package com.heima.article.job;
+
+import com.heima.article.service.HotArticleService;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j
+public class ComputeHotArticleJob {
+
+    @Autowired
+    private HotArticleService hotArticleService;
+
+    @XxlJob("computeHotArticleJob")
+    public void handle(){
+        log.info("热文章分值计算调度任务开始执行...");
+        hotArticleService.computeHotArticle();
+        log.info("热文章分值计算调度任务结束...");
+
+    }
+}
